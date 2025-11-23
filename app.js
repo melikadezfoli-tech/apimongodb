77253089f1c35bc298a7e3efa0792a556627730d
Melikadezfoli-Tech <melikadezfoli@gmail.com>
2025-11-23 18:26:43 +0100

    views

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-//console.log('Hola mundo');
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html')
+})
---
+++ mimodulo.js
@@ -0,0 +1,9 @@
+const saludar=(nombre)=>{
+    return `Hola ${nombre}`;
+};
+
+const despedir=(nombre)=>{
+    return `Adiós ${nombre}!`;
+};
+
+module.exports = {saludar, despedir};
